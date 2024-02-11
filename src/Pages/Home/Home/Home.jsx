cd0959e420f66595a261cad6b3e4316cb469da94
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-02-11 07:50:01 +0600

    slider of home page running

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,9 @@
+import HomeBanner from "../HomeBanner/HomeBanner";
+
 const Home = () => {
   return (
     <div className="bg-red-300 h-40">
+      <HomeBanner></HomeBanner>
       <p className="text-center">This is home</p>
     </div>
   );
